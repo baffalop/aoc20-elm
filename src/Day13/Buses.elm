b95004b5335183994cc8e3f5204065286ca43229
Nikita Gaidakov <ngaidakov@podfather.com>
2020-12-13 23:41:21 +0000

    An attempt at day 13 part 2. Maybe if the universe lived a bit longer...

--- src/Day13/Buses.elm
+++ src/Day13/Buses.elm
@@ -5,7 +5,7 @@ import List.Extra
 
 solve1 : String -> Maybe Int
 solve1 =
-    parse
+    parse1
         >> Maybe.andThen
             (\{ start, buses } ->
                 buses
@@ -20,14 +20,43 @@ solve1 =
             )
 
 
-type alias Constraints =
+solve2 : String -> Maybe Int
+solve2 =
+    parse2
+        >> toConstraints
+        >> (\constraints ->
+                List.Extra.maximumBy .id constraints
+                    |> Maybe.map
+                        (\max ->
+                            List.map (\c -> { c | offset = c.offset - max.offset }) constraints
+                                |> satisfy max.id 1
+                        )
+           )
+
+
+satisfy : Int -> Int -> List Constraint -> Int
+satisfy bus mult input =
+    if List.all (\{ id, offset } -> (((bus * mult) + offset) |> remainderBy id) == 0) input then
+        bus * mult
+
+    else
+        satisfy bus (mult + 1) input
+
+
+type alias Schedule =
     { start : Int
     , buses : List Int
     }
 
 
-parse : String -> Maybe Constraints
-parse input =
+type alias Constraint =
+    { id : Int
+    , offset : Int
+    }
+
+
+parse1 : String -> Maybe Schedule
+parse1 input =
     case String.lines input of
         line1 :: line2 :: [] ->
             String.toInt line1
@@ -43,6 +72,40 @@ parse input =
 
         _ ->
             Nothing
+
+
+parse2 : String -> List (Maybe Int)
+parse2 input =
+    case String.lines input of
+        _ :: line2 :: [] ->
+            line2
+                |> String.split ","
+                |> List.map String.toInt
+
+        _ ->
+            []
+
+
+toConstraints : List (Maybe Int) -> List Constraint
+toConstraints =
+    let
+        process : Int -> List (Maybe Int) -> List Constraint
+        process lastOffset input =
+            case List.Extra.splitWhen ((/=) Nothing) input of
+                Just ( xs, (Just bus) :: rest ) ->
+                    let
+                        offset =
+                            lastOffset + List.length xs
+                    in
+                    { id = bus
+                    , offset = offset
+                    }
+                        :: process offset rest
+
+                _ ->
+                    []
+    in
+    process 0
 
 
 puzzleInput =
