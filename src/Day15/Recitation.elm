6c4b68ec9fde74fa7f40abea38b2a28a3b460237
Nikita Gaidakov <ngaidakov@podfather.com>
2020-12-15 20:58:17 +0000

    Generate whole list and take last element

--- src/Day15/Recitation.elm
+++ src/Day15/Recitation.elm
@@ -1,4 +1,4 @@
-module Day15.Recitation exposing (puzzleInput, solve1, solve2)
+module Day15.Recitation exposing (..)
 
 import Basics.Extra exposing (flip, uncurry)
 import Dict exposing (Dict)
@@ -7,17 +7,18 @@ import List.Extra
 
 solve1 : String -> Maybe Int
 solve1 =
-    parse >> playUntil 2020
+    parse >> playUntil 2020 >> Maybe.andThen List.Extra.last
 
 
+solve2 : String -> Maybe Int
 solve2 =
-    parse >> playUntil 30000000
+    parse >> playUntil 30000000 >> Maybe.andThen List.Extra.last
 
 
-playUntil : Int -> List Int -> Maybe Int
+playUntil : Int -> List Int -> Maybe (List Int)
 playUntil limit input =
     let
-        play : Int -> Int -> Dict Int Int -> Int
+        play : Int -> Int -> Dict Int Int -> List Int
         play index lastTurn memory =
             let
                 thisTurn =
@@ -25,11 +26,11 @@ playUntil limit input =
                         |> Maybe.map ((-) (index - 1))
                         |> Maybe.withDefault 0
             in
-            if index == limit then
-                thisTurn
+            if index > limit then
+                []
 
             else
-                play (index + 1) thisTurn (Dict.insert lastTurn (index - 1) memory)
+                thisTurn :: play (index + 1) thisTurn (Dict.insert lastTurn (index - 1) memory)
     in
     List.Extra.unconsLast input
         |> Maybe.map
@@ -37,6 +38,7 @@ playUntil limit input =
                 List.indexedMap ((+) 1 >> flip Tuple.pair) rest
                     |> List.foldl (uncurry Dict.insert) Dict.empty
                     |> play (List.length input + 1) lastInput
+                    |> (++) input
             )
 
 
